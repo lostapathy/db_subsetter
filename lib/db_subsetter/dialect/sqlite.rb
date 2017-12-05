e9e8cf2bed4a28f22641db741de4584a5b6044d8
Joe Francis <joe@lostapathy.com>
2017-12-04 23:53:40 -0600

    sqlite can check for foreign key violations

--- lib/db_subsetter/dialect/sqlite.rb
+++ lib/db_subsetter/dialect/sqlite.rb
@@ -2,6 +2,9 @@ module DbSubsetter
   module Dialect
     # Dialect to subset to/from sqlite
     class Sqlite < Generic
+      def self.integrity_problems
+        ActiveRecord::Base.connection.execute('PRAGMA foreign_key_check')
+      end
     end
   end
 end
